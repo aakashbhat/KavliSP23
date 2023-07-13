66ed90fa5102bf5ddd1960c3ff1d0560e60d6827
aakashbhat <aakashbhat7@gmail.com>
2023-07-13 17:17:36 +0200

    new evolved models

--- Test1/Evolve/plot.py
+++ Test1/Evolve/plot.py
@@ -4,32 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 # load and plot data
 h = mr.MesaData('LOGS_evolved_1year/profile4.data')
-plot="rhoT"
+plot="composition"
 if plot=="composition":
     h2 = np.genfromtxt('composition.dat',skip_header=1)
-    plt.plot(h.xq, np.log10(h.h1))
     plt.plot(h.xq, np.log10(h.he4))
     plt.plot(h.xq, np.log10(h.c12))
     plt.plot(h.xq, np.log10(h.n14))
     plt.plot(h.xq, np.log10(h.o16))
-    plt.plot(h.xq, np.log10(h.ni56))
+    plt.plot(h.xq, np.log10(h.fe56))
     plt.ylim(-5,0)
-    plt.plot(h2[:,0],np.log10(h2[:,3]))
-    plt.plot(h2[:,0],np.log10(h2[:,4]))
-    plt.plot(h2[:,0],np.log10(h2[:,5]))
-    plt.plot(h2[:,0],np.log10(h2[:,6]))
-    plt.plot(h2[:,0],np.log10(h2[:,12]))
+    #plt.plot(h2[:,0],np.log10(h2[:,3]))
+    #plt.plot(h2[:,0],np.log10(h2[:,4]))
+    #plt.plot(h2[:,0],np.log10(h2[:,5]))
+    #plt.plot(h2[:,0],np.log10(h2[:,6]))
+    #plt.plot(h2[:,0],np.log10(h2[:,12]))
 
     # set axis labels
-    plt.legend(("h1","he4","c12","n14","o16","ni56","he4R","c12r","n14r","o16r","ni56r"))
+    plt.legend(("he4","c12","n14","o16","fe56"))
+    #,"he4R","c12r","n14r","o16r","fe56r"))
 
-    plt.xlabel(r'$X_q$')
+    plt.xlabel(r'log$X_q$')
     plt.ylabel(r'$X_n$')
 
     # invert the x-axis
     #plt.gca().invert_xaxis()
-    plt.savefig("Plots/composition_comparison_evolved_10.pdf")
-    plt.savefig("Plots/composition_comparison_evolved_10.png")
+    plt.savefig("Plots/composition_comparison_evolved_1.pdf")
+    plt.savefig("Plots/composition_comparison_evolved_1.png")
 
 
 elif plot=="rhoT":
---
+++ Test1/Long_Evolve/plot.py
@@ -0,0 +1,54 @@
+# import mesa_reader
+import mesa_reader as mr
+import matplotlib.pyplot as plt
+import numpy as np
+# load and plot data
+h = mr.MesaData('LOGS_evolved_1D8years/profile29.data')
+plot="composition"
+if plot=="composition":
+    h2 = np.genfromtxt('composition.dat',skip_header=1)
+    plt.plot(h.xq, np.log10(h.he4))
+    plt.plot(h.xq, np.log10(h.c12))
+    plt.plot(h.xq, np.log10(h.n14))
+    plt.plot(h.xq, np.log10(h.o16))
+    plt.plot(h.xq, np.log10(h.fe56))
+    plt.ylim(-15,0)
+    plt.plot(h2[:,0],np.log10(h2[:,3]))
+    plt.plot(h2[:,0],np.log10(h2[:,4]))
+    plt.plot(h2[:,0],np.log10(h2[:,5]))
+    plt.plot(h2[:,0],np.log10(h2[:,6]))
+    plt.plot(h2[:,0],np.log10(h2[:,12]))
+
+    # set axis labels
+    plt.legend(("he4","c12","n14","o16","fe56","he4R","c12r","n14r","o16r","fe56r"))
+
+    plt.xlabel(r'log$X_q$')
+    plt.ylabel(r'$X_n$')
+
+    # invert the x-axis
+    #plt.gca().invert_xaxis()
+    plt.savefig("Plots/composition_comparison_evolved_1d8.pdf")
+    plt.savefig("Plots/composition_comparison_evolved_1d8.png")
+
+
+elif plot=="rhoT":
+    h3 = np.genfromtxt('entropy.dat',skip_header=1)
+    plt.plot(h.logRho, h.logT)
+    plt.plot(np.log10(h3[:,1]),np.log10(h3[:,2]))
+    plt.xlabel(r"log$\rho$")
+    plt.ylabel("logT")
+    plt.show()
+        #plt.gca().invert_xaxis()
+
+elif plot=="HR":
+    # load and plot data
+    h = mr.MesaData('LOGS_evolved_1D8years/history.data')
+    plt.plot(h.log_Teff, h.log_L)
+
+    # set axis labels
+    plt.xlabel('log Effective Temperature')
+    plt.ylabel('log Luminosity')
+
+    # invert the x-axis
+    plt.gca().invert_xaxis()
+    plt.show()
