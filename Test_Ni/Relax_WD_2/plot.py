07f246272b3e2853de7556d537600662dab7882a
aakashbhat <aakashbhat7@gmail.com>
2023-09-20 16:38:58 +0200

    final dump before starting new

--- Test_Ni/Relax_WD_2/plot.py
+++ Test_Ni/Relax_WD_2/plot.py
@@ -7,12 +7,14 @@ h = mr.MesaData('LOGS_relaxed_ni56_normalh_he4/profile1.data')
 plot="rhoT"
 if plot=="composition":
     h2 = np.genfromtxt('composition.dat',skip_header=1)
-    plt.plot(h.xq, np.log10(h.h1))
-    plt.plot(h.xq, np.log10(h.he4))
-    plt.plot(h.xq, np.log10(h.c12))
-    plt.plot(h.xq, np.log10(h.n14))
-    plt.plot(h.xq, np.log10(h.o16))
-    plt.plot(h.xq, np.log10(h.ni56))
+    h4 = mr.MesaData('../../Test1/make_co_wd/LOGS/profile34.data')
+
+    plt.plot(h4.xq, np.log10(h4.h1))
+    plt.plot(h4.xq, np.log10(h4.he4))
+    plt.plot(h4.xq, np.log10(h4.c12))
+    plt.plot(h4.xq, np.log10(h4.n14))
+    plt.plot(h4.xq, np.log10(h4.o16))
+    #plt.plot(h4.xq, np.log10(h4.ni56))
     plt.ylim(-5,0)
     plt.plot(h2[:,0],np.log10(h2[:,3]))
     plt.plot(h2[:,0],np.log10(h2[:,4]))
@@ -21,28 +23,43 @@ if plot=="composition":
     plt.plot(h2[:,0],np.log10(h2[:,12]))
 
     # set axis labels
-    plt.legend(("h1","he4","c12","n14","o16","ni56","he4R","c12r","n14r","o16r","ni56r"))
+    plt.legend(("h1o","he4o","c12o","n14o","o16o","he4r","c12r","n14r","o16r","ni56r"))
 
     plt.xlabel(r'$X_q$')
     plt.ylabel(r'$X_n$')
 
     # invert the x-axis
     #plt.gca().invert_xaxis()
-    plt.savefig("Plots/composition_comparison_evolved_10.pdf")
-    plt.savefig("Plots/composition_comparison_evolved_10.png")
+    plt.savefig("Plots/composition_comparison_withWD.pdf")
+    plt.savefig("Plots/composition_comparison_withWD.png")
 
 
 elif plot=="rhoT":
-    h3 = np.genfromtxt('entropy.dat',skip_header=1)
+    #h2 = np.genfromtxt('donor_entropy.dat',skip_header=1)
+    fig,ax=plt.subplots()
+    plt.rcParams["font.size"]=20
+    plt.rcParams["axes.labelsize"]=20
+    plt.rcParams["legend.fontsize"]=15
+    plt.rcParams['lines.linestyle']="--"
+    plt.rcParams['lines.linewidth']=4
+    #img = plt.imread("eos_regions.jpg")
+    #ax.imshow(img,extent=[])
     plt.plot(h.logRho, h.logT)
+    h3 = np.genfromtxt('entropy.dat',skip_header=1)
     h4 = mr.MesaData('../../Test1/make_WD_model/LOGS/profile2.data')
     plt.plot(h4.logRho, h4.logT)
 
+    #plt.plot(np.log10(h2[:,1]),np.log10(h2[:,2]))
     plt.plot(np.log10(h3[:,1]),np.log10(h3[:,2]))
-    plt.xlabel(r"log$\rho$")
-    plt.ylabel("logT")
-    plt.legend(['Relaxed WD','Original WD','Simulation'])
-    plt.savefig("Plots/entropy_relaxed_064_normalh_he4_comparisonwithWD.png")
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlabel(r"log$\rho$ (gcm$^{-3}$)",fontsize=18)
+    plt.ylabel("logT (K)",fontsize=18)
+    plt.legend(['Relaxed WD','Original WD','Arepo Model'])
+    plt.tight_layout()
+    plt.savefig("/userdata/data/bhat/D6/KavliSP23/Report/Plots/entropy_comparison_relaxation.pdf")
+    plt.savefig("/userdata/data/bhat/D6/KavliSP23/Report/Plots/entropy_comparison_relaxation.png")
+
         #plt.gca().invert_xaxis()
 
 elif plot=="profile":
---
+++ Test_Ni_Opacity_noHe/readme.txt
@@ -0,0 +1,1 @@
+In this folder I test models without any Helium content
